9832dda81b54097221564329dada1e7bf6caf2f8
ankit10000 <ankitjangid9197@gmail.com>
2024-06-14 11:33:57 +0530

    feat: Add @tailwindcss/forms for improved form styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
       <Dashboard />
+      <Footer />
     </div>
   );
 }
