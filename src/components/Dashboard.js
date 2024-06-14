73674b527074cda7bfab64dfde38d01d49bc6477
ankit10000 <ankitjangid9197@gmail.com>
2024-06-14 11:08:11 +0530

    chore: Update npm dependencies and add Tailwind CSS

---
+++ src/components/Dashboard.js
@@ -0,0 +1,65 @@
+
+const callouts = [
+    {
+      name: 'PROFESSIONAL LOAN',
+      description: 'A professional loan is a type of loan designed for self-employed professionals such as doctors, lawyers, and architects. It provides funding for various professional needs, including setting up a practice, purchasing equipment, or managing working capital.',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+      href: '#',
+    },
+    {
+      name: 'PERSONAL LOAN',
+      description: 'A personal loan is an unsecured loan provided by financial institutions based on an applicants creditworthiness and income. It can be used for various personal expenses such as medical bills, home renovations, or vacations',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+      href: '#',
+    },
+    {
+      name: 'OVERDRAFT',
+      description: 'An overdraft is a facility offered by banks that allows account holders to withdraw more money than what is available in their account up to a certain limit. It is typically used to cover short-term financial shortfalls',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+      href: '#',
+    },
+    {
+        name: 'BUISNESS LOAN',
+        description: 'A business loan is a loan specifically intended for business purposes, such as starting a new business, expanding an existing one, or covering operational costs. These loans can be secured or unsecured, depending on the lenders requirements.',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+        href: '#',
+      },
+  ]
+  
+  export default function Example() {
+    return (
+      <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+            <h2 className="text-2xl font-bold text-gray-900">Loans Category</h2>
+  
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+              {callouts.map((callout) => (
+                <div key={callout.name} className="group relative">
+                  <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                    <img
+                      src={callout.imageSrc}
+                      alt={callout.imageAlt}
+                      className="h-full w-full object-cover object-center"
+                    />
+                  </div>
+                  <h3 className="mt-6 text-sm text-gray-500">
+                    <a href={callout.href}>
+                      <span className="absolute inset-0" />
+                      {callout.name}
+                    </a>
+                  </h3>
+                  <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
